b1a8b79783d556a33f90129faf790da62f75f733
alexharp <aharper488@outlook.com>
2020-05-15 06:21:50 +0000

    finished graph_query_form first run, waiting on completion of SPARQL queries

--- web/modules/custom/neptune_sync/src/Form/LocalGraphForm.php
+++ web/modules/custom/neptune_sync/src/Form/LocalGraphForm.php
@@ -6,6 +6,7 @@ namespace Drupal\neptune_sync\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\neptune_sync\Utility\Helper;
+use Drupal\node\NodeInterface;
 
 class LocalGraphForm extends FormBase {
 
@@ -15,25 +16,12 @@ class LocalGraphForm extends FormBase {
         return 'local_graph_form';
     }
 
-    public function buildForm(array $form, FormStateInterface $form_state, $node = null)
+    public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = null)
     {
-        $x =\Drupal\Webform\Entity\Webform::load('local_grapgh_query');
-        //kint($x, 'pre getSubmissionForm');
-        $y = \Drupal::entityManager()
-            ->getViewBuilder('webform')
-            ->view($x);
-        //kint($y, 'form view ');
-
-
-       /* $form['elements'] = $y['elements'];
-
-        $form['actions']['#type'] = 'actions';
-        $form['actions']['submit'] = [
-            '#type' => 'submit',
-            '#value' => $this->t('Save'),
-            '#button_type' => 'primary',
+        $form['markup'] = [
+            '#type' => 'webform_markup',
+            '#markup' => t("Query starting at @nodetitle", array('@nodetitle' => $node->getTitle())),
         ];
-        kint($form);*/
 
         $form['size_of_local_graph'] = [
             '#type' => 'select',
@@ -84,16 +72,17 @@ class LocalGraphForm extends FormBase {
             ],
             //TODO add this later '#default_value' => $config->get('ignore_relationship_types_of'),
         ];
+        //TODO fix date default values and min/max values
         $form['restrict_results_from_'] = [
             '#type' => 'date',
             '#title' => $this->t('Restrict results from '),
             '#help' => $this->t('Provide the start of a range that results must fall under if they have a date attached. This field is optional '),
             '#default_value' => array(
-                'year' => 1901,
-                'month' => 1,
                 'day' => 15,
+                'month' => 01,
+                'year' => 1901,
             ),
-            '#date_date_min' => '01-01-1901',
+            //'#date_date_min' => '01-01-1901',
             '#datepicker' => TRUE,
             '#datepicker_button' => TRUE,
         ];
@@ -106,6 +95,8 @@ class LocalGraphForm extends FormBase {
             '#datepicker' => TRUE,
             '#datepicker_button' => TRUE,
         ];
+
+
         $form['actions'] = [
             '#type' => 'actions',
             '#tree' => TRUE,
@@ -116,32 +107,6 @@ class LocalGraphForm extends FormBase {
             '#button_type' => 'primary',
         ];
 
-        // Process elements.
-        //$this->elementManager->processElements($form);
-
-        // Replace tokens.
-       // $form = $this->tokenManager->replace($form);
-
-       /* // Attach the webform library.
-        $form['#attached']['library'][] = 'webform/webform.form';
-
-        // Autofocus: Save details open/close state.
-        $form['#attributes']['class'][] = 'js-webform-autofocus';
-        $form['#attached']['library'][] = 'webform/webform.form.auto_focus';
-
-        // Unsaved: Warn users about unsaved changes.
-        $form['#attributes']['class'][] = 'js-webform-unsaved';
-        $form['#attached']['library'][] = 'webform/webform.form.unsaved';
-
-        // Details save: Attach details element save open/close library.
-        $form['#attached']['library'][] = 'webform/webform.element.details.save';
-
-        // Details toggle: Display collapse/expand all details link.
-        $form['#attributes']['class'][] = 'js-webform-details-toggle';
-        $form['#attributes']['class'][] = 'webform-details-toggle';
-        $form['#attached']['library'][] = 'webform/webform.element.details.toggle';*/
-
-
         return $form;
     }
 
@@ -151,8 +116,7 @@ class LocalGraphForm extends FormBase {
     }
 
     public function  submitForm(array &$form, FormStateInterface $form_state){
-        kint($form_state);
-        kint($form_state->getValues());
-        $this->messenger()->addStatus($this->t('Your phone number is @number', ['@number' => $form_state->getValues()]));
+
+        $values = $form_state->getValues();
     }
 }
