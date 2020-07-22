688ac7d92cf5493b279d0674d97457b320ef051b
alexharp <aharper488@outlook.com>
2020-07-22 05:14:27 +0000

    rewrote the graph import script to work with args and graph select

--- web/scripts/import-neptune.sh
+++ web/scripts/import-neptune.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-curl -X POST \
-    -H 'Content-Type: application/json' \ 
-    https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/loader -d '
-    {
-      "source" : "s3://drupal-sophia-store/Migration/20200505/SillyWalks.ttl",
-      "format" : "turtle",
-      "iamRoleArn" : "arn:aws:iam::075128907886:role/NeptuneLoadFromS3",
+QUERY+='{
+"source" : "s3://drupal-sophia-store/'
+#Migration/20200505/SillyWalks.ttl",
+QUERY+=${1} #filename
+QUERY+='", '
+QUERY+='"format" : "'
+#'turtle",
+QUERY+=${2} #file type
+QUERY+='", "iamRoleArn" : "arn:aws:iam::075128907886:role/NeptuneLoadFromS3",
       "region" : "ap-southeast-2",
       "failOnError" : "FALSE",
       "parallelism" : "MEDIUM",
       "updateSingleCardinalityProperties" : "FALSE",
-      "queueRequest" : "TRUE"
+      "queueRequest" : "TRUE",
+      "parserConfiguration" : {
+        "namedGraphUri" : "http://aws.amazon.com/neptune/vocab/v00'
+QUERY+=${3} #single digit graph no
+QUERY+='"
+      }
     }'
 
-#curl -G 'https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/loader/$LOAD_ID'
+echo "Executing: "
+echo $QUERY
+
+curl -X POST -H 'Content-Type: application/json' https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/loader -d "$QUERY"
+
+echo "Run the cmd below to check status"
+echo "curl -G 'https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/loader/"
+
 #https://docs.aws.amazon.com/neptune/latest/userguide/bulk-load-data.html
