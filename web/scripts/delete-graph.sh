cc5bdcc04a8c28d858aa506d6b395353284da717
alexharp <aharper488@outlook.com>
2020-10-22 01:12:44 +0000

    made delete graph generic

--- web/scripts/delete-graph.sh
+++ web/scripts/delete-graph.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/sparql -d "update=CLEAR GRAPH <http://aws.amazon.com/neptune/vocab/v001>" 
+
+QUERY+="update=CLEAR GRAPH <http://aws.amazon.com/neptune/vocab/v00"
+QUERY+=${1}
+QUERY+=">" 
+
+echo "Executing: "
+echo $QUERY
+curl https://sophia-neptune.cbkhatvpiwzj.ap-southeast-2.neptune.amazonaws.com:8182/sparql -d "$QUERY" 
