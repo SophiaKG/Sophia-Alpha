c30c318d5313bd0e418a825324f9fb05dae56154
alexharp <aharper488@outlook.com>
2020-10-23 00:34:40 +0000

    shameful fix for lead bodies to appear on summary vie

--- web/modules/custom/neptune_sync/src/Data/CharacterSheetManager.php
+++ web/modules/custom/neptune_sync/src/Data/CharacterSheetManager.php
@@ -47,6 +47,13 @@ class CharacterSheetManager
         $this->processEmploymentType($node);
         $this->processCooperativeRelationships($node, $bulkOperation);
 
+
+        //TODO THIS NEEDS REPLACING! This forces lead bodies to appear on the summary view
+        if(strtoupper($node->getTitle()) == $node->getTitle()){
+            Helper::log($this->body->getLabelKey() . " detected as lead body, ensuring it appears
+            on the summary view");
+            $this->body->setTypeOfBody(87);
+        }
         try {
             $this->updateNode($node);
         } catch (EntityStorageException|MissingDataException $e) {
