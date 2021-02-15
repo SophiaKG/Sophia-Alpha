beded112c521b78c3d69f83b71624313e5478103
alexharp <aharper488@outlook.com>
2021-02-15 06:19:58 +0000

    Changes fot open release ready front page including hoomeppage swapping basd on role

---
+++ web/modules/custom/neptune_sync/src/Event/HomepageRedirectionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+/***
+ * on going to <front> if you are not anonymous, redirect
+ * Source: https://drupal.stackexchange.com/questions/288707/different-front-page-for-logged-in-users
+ * @todo migrate out oof module
+ * @uses neptune_sync.services.yml.event_subscriber
+ * @author Alexis harper | DoF
+ */
+
+namespace Drupal\neptune_sync\Event;
+
+use Drupal\neptune_sync\Utility\Helper;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class HomepageRedirectionSubscriber implements EventSubscriberInterface {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkFrontRedirection(GetResponseEvent $event) {
+        if (\Drupal::service('path.matcher')->matchPath(
+                    \Drupal::service('path.current')->getPath(),'/node/43151') &&
+                !\Drupal::currentUser()->isAnonymous()) {
+            
+            $path =  \Drupal::service('path.alias_manager')->getAliasByPath('/drupal8/web/welcome-beta');
+            $event->setResponse(new RedirectResponse($path));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents() {
+        $events[KernelEvents::REQUEST][] = array('checkFrontRedirection');
+        return $events;
+    }
+
+}
