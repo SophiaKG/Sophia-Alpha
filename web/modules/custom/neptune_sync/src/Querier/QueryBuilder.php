460fe0183d11db018a546d89bfa7d58a8c594b50
alexharp <aharper488@outlook.com>
2021-03-16 05:43:40 +0000

    modified portfolio query to get all portfolios, not just flip chart portfolios

--- web/modules/custom/neptune_sync/src/Querier/QueryBuilder.php
+++ web/modules/custom/neptune_sync/src/Querier/QueryBuilder.php
@@ -96,9 +96,10 @@ class QueryBuilder {
                 '?auth ns2:fallsUnder ?port. ' .
                 '?port rdfs:label ?portlabel. ' .
                 '?auth ns2:binds ' . self::getUri($node, 'ns2') . '. ' .
-                '?flip ns2:witnesses ?auth. ' .
-                '?flip a ns2:FinanceEntityList. ' .
-                '?flip ns2:live true. ' .
+                '?port ns2:live true. ' .
+                //'?flip ns2:witnesses ?auth. ' .
+                //'?flip a ns2:FinanceEntityList. ' .
+                //'?flip ns2:live true. ' .
             '} ORDER BY DESC(?datetime) ' .
             'LIMIT 1');
 
