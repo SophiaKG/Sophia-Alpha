4201ed40e4e5ae18dd3713b02c42e1265b8b329a
Qing Li (Richard) <qing.li@finance.gov.au>
2023-02-07 12:14:12 +1100

    [feat/outcome-graph-echarts] Update the outcome programs graph and switch the graph engine from d3-graphviz to Apache Echarts.

--- web/modules/custom/fkg_ct_outcome/js/outcome-graph-echarts.js
+++ web/modules/custom/fkg_ct_outcome/js/outcome-graph-echarts.js
@@ -2,4 +2,65 @@
  * @file
  */
 
-(function ($, window, Drupal) {})(jQuery, window, Drupal);
+(function ($, window, Drupal) {
+  Drupal.behaviors.fkgGraphOutcomeProgram = {
+    attach: function (context, drupalSettings) {
+      $('.fkg-graph-echarts').each(function (){
+        var outcomeId = $(this).data('outcome-id');
+        // @see PHP function _fkg_ct_outcome_get_contributing_programs_graph().
+        var fkgGraphData = drupalSettings['fkg-graph'][outcomeId];
+
+        var graph = echarts.init(this, null, {
+          renderer: 'svg',
+          width: 900,
+          height: 600,
+        });
+
+        graphOption = {
+          animation: false,
+          title: {
+            text: fkgGraphData.title,
+            top: 0,
+            left: 'center',
+          },
+          legend: [{
+            selectedMode: 'multiple',
+            orient: 'vertical',
+            left: 0,
+            top: '8%',
+            width: 200,
+            data: fkgGraphData.categories.map(
+              function (item) {return item.name;}
+            ),
+            textStyle: {
+              width: 200,
+              overflow: 'break',
+            },
+          }],
+          series: [{
+            name: fkgGraphData.title,
+            type: 'graph',
+            layout: 'force',
+            data: fkgGraphData.nodes,
+            links: fkgGraphData.links,
+            categories: fkgGraphData.categories,
+            label: {
+              show: false,
+              fontSize: 15
+            },
+            labellayout: {
+              hideOverlap: true
+            },
+            force: {
+              repulsion: 500,
+              layoutAnimation: false
+            },
+          }],
+        };
+
+        graph.setOption(graphOption);
+      });
+    }
+  };
+})(jQuery, window, Drupal);
+
